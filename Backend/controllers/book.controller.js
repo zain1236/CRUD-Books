f54e48680f5738ce0fed54a0749445d4e9863d3d
zain <saimzain1236@gmail.com>
2023-04-18 02:29:03 +0500

    Backend complete

--- Backend/controllers/book.controller.js
+++ Backend/controllers/book.controller.js
@@ -1,15 +1,29 @@
 const model = require("../models")
-const Joi = require("joi")
+const Joi = require("joi");
+const { date } = require("joi");
 
 
 const bookSchema = Joi.object({
-    tit: Joi.number()
+    title: Joi.string()
         .required(),
+
+    author : Joi.string()
+        .required(),
+
+    no_of_pages : Joi.number()
+        .required(),
+    
+    published_at : Joi.date()
+        .required()
+});
+
+const bookIdSchema =  Joi.object({
+    _id : Joi.string()
+        .required()
 });
 
 exports.create = async (req, res) => {
     try {
-        console.log("request received");
         if (!req.body) {
             res.status(400).send({
                 message: "Content can not be empty!"
@@ -17,12 +31,79 @@ exports.create = async (req, res) => {
             return;
         }
 
-        console.log(req.body);
-        res.status(200).send("ok")
-        // const book = new model.Book(req.body);        
+        const bookValidate = await bookSchema.validateAsync(req.body);
+        const book = new model.Book(bookValidate);
+        const result = await book.save();
+        res.status(200).send({"Message" : "success","data" : result})            
     }
     catch (error) {
         console.log("In Catch..");
         res.status(400).send({ "Message": error.message});
     }
 };
+
+exports.getAll = async (req, res) => {
+    try {
+        const books = await model.Book.find();
+        res.status(200).send({"Message" : "success","data" : books})            
+    }
+    catch (error) {
+        console.log("In Catch..");
+        res.status(400).send({ "Message": error.message});
+    }
+};
+
+exports.getOne = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const books = await model.Book.findById(id);
+        if (books){
+            res.status(200).send({"Message" : "success","data" : books})            
+        } else {
+            res.status(404).send({"Message" : "Book not found"})            
+        }
+    }
+    catch (error) {
+        console.log("In Catch..");
+        res.status(400).send({ "Message": error.message});
+    }
+};
+
+exports.delete = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const books = await model.Book.findByIdAndDelete(id);
+
+        if (books){
+            res.status(200).send({"Message" : "success","data" : books})            
+        } else {
+            res.status(404).send({"Message" : "Book not found"})            
+        }
+    }
+    catch (error) {
+        console.log("In Catch..");
+        res.status(400).send({ "Message": error.message});
+    }
+};
+
+exports.update = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const books = await model.Book.findByIdAndUpdate(id,{...req.body,updated_at: Date.now()});
+
+        if (books){
+            res.status(200).send({"Message" : "success","data" : books})            
+        } else {
+            res.status(404).send({"Message" : "Book not found"})            
+        }
+    }
+    catch (error) {
+        console.log("In Catch..");
+        res.status(400).send({ "Message": error.message});
+    }
+};
+
+
+
+
+
--- Backend/routes/book.routes.js
+++ Backend/routes/book.routes.js
@@ -5,6 +5,10 @@ var controller = require("../controllers")
 
 
 router.post("/",controller.Book.create)
+router.get("/:id",controller.Book.getOne)
+router.get("/",controller.Book.getAll)
+router.delete("/:id",controller.Book.delete)
+router.put("/:id",controller.Book.update)
 
 
 module.exports = router;
